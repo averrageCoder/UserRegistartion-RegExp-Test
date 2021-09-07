291340c48a49f6024f35abe5d4afffc864316af4
Harsh R Jain <rjainharsh@gmail.com>
2021-09-07 20:29:30 +0530

    ADDED: validation for last name

--- src/com/bridgelabz/userregistrationregex/UserRegistartionRegex.java
+++ src/com/bridgelabz/userregistrationregex/UserRegistartionRegex.java
@@ -10,6 +10,26 @@ public class UserRegistartionRegex {
 	
 	public static void main(String[] args) {
 		firstNameValidator();
+		lastNameValidator();
+	}
+
+	private static void lastNameValidator() {
+		
+		Pattern firstNamePattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		Matcher matcher;
+		
+		System.out.println("\nEnter last name: ");
+		String firstName = "Jain"; //scan.nextLine();
+		
+		matcher = firstNamePattern.matcher(firstName);
+		boolean matchFound = matcher.find();
+		if(matchFound) {
+			System.out.println(firstName+" -\t Valid");
+		} 
+		else {
+			System.out.println(firstName+" -\t Invalid");
+		}
+		
 	}
 
 	private static void firstNameValidator() {
