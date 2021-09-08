8dfd9934415b4d49c6ba8d9fd581ddcce2392bc7
Harsh R Jain <rjainharsh@gmail.com>
2021-09-08 21:44:50 +0530

    ADDED: unit testing for first last name, email, phone, password

--- src/com/bridgelabz/userregistrationregex/UserRegistartionRegex.java
+++ src/com/bridgelabz/userregistrationregex/UserRegistartionRegex.java
@@ -1,136 +1,46 @@
 package com.bridgelabz.userregistrationregex;
-
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistartionRegex {
-	
-	//static Scanner scan = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		firstNameValidator();
-		lastNameValidator();
-		emailValidator();
-		mobilePhoneValidator();
-		passwordValidator();
-	}
-
-	private static void passwordValidator() {
+	public  boolean passwordValidator(String password) {
 		
 		Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[\\S]{8,}$");
 		Matcher matcher;
 		
-		System.out.println("\nEnter password: ");
-		String password = "s9dSs%sd"; //scan.nextLine();
-		
 		matcher = passwordPattern.matcher(password);
 		boolean matchFound = matcher.find();
-		if(matchFound) {
-			System.out.println(password+" -\t Valid");
-		} 
-		else {
-			System.out.println(password+" -\t Invalid");
-		}
-		
-		
+		return matchFound;		
 	}
 
-	private static void mobilePhoneValidator() {
+	public boolean mobilePhoneValidator(String phoneNumber) {
 		
 		Pattern phonePattern = Pattern.compile("^[0-9]{2}\\s[0-9]{10}$");
 		Matcher matcher;
 		
-		System.out.println("\nEnter email: ");
-		String phoneNumber = "91 9876543210"; //scan.nextLine();
-		
 		matcher = phonePattern.matcher(phoneNumber);
 		boolean matchFound = matcher.find();
-		if(matchFound) {
-			System.out.println(phoneNumber+" -\t Valid");
-		} 
-		else {
-			System.out.println(phoneNumber+" -\t Invalid");
-		}
-		
+		return matchFound;		
 	}
 
-	private static void emailValidator() {
+	public boolean emailValidator(String email) {
 		
-		Pattern emailPattern = Pattern.compile("^abc([.+_-][A-Z0-9]+)?[A-Z0-9]*[@][A-Z0-9]+.[A-Z]{2,}(.[A-Z]{2,6})?$",Pattern.CASE_INSENSITIVE);
+		Pattern emailPattern = Pattern.compile("^abc([\\.\\+\\_\\-][A-Z0-9]+)?[A-Z0-9]*[@][A-Z0-9]+.[A-Z]{2,}(.[A-Z]{2,6})?$",Pattern.CASE_INSENSITIVE);
 		Matcher matcher;
 		
-		System.out.println("\nEnter email: ");
-		String validEmails[] = {
-				"abc-100@yahoo.com","abc@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net",
-				"abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"
-		};
-		String invalidEmails[] = {
-				"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com",
-				"abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"
-		};
-		
-		for(String email: validEmails) {
-			
-			 matcher = emailPattern.matcher(email);
-			 boolean matchFound = matcher.find();
-				if(matchFound) {
-					System.out.println(email+"-\tValid");
-				} 
-				else {
-					System.out.println(email+"-\tInvalid");
-				}
-		}
-		System.out.println("\n");
-		for(String email: invalidEmails) {
-			
-			 matcher = emailPattern.matcher(email);
-			 boolean matchFound = matcher.find();
-				if(matchFound) {
-					System.out.println(email+"-\tValid");
-				} 
-				else {
-					System.out.println(email+"-\tInvalid");
-				}
-		}
-		
-	}
-
-	private static void lastNameValidator() {
-		
-		Pattern lastNamePattern = Pattern.compile("^[A-Z][a-z]{2,}");
-		Matcher matcher;
-		
-		System.out.println("\nEnter last name: ");
-		String lastName = "Jain"; //scan.nextLine();
-		
-		matcher = lastNamePattern.matcher(lastName);
+		matcher = emailPattern.matcher(email);
 		boolean matchFound = matcher.find();
-		if(matchFound) {
-			System.out.println(lastName+" -\t Valid");
-		} 
-		else {
-			System.out.println(lastName+" -\t Invalid");
-		}
-		
+		return matchFound;
 	}
 
-	private static void firstNameValidator() {
+	public boolean firstNameLastNameValidator(String name) {
 		
 		Pattern firstNamePattern = Pattern.compile("^[A-Z][a-z]{2,}");
 		Matcher matcher;
 		
-		System.out.println("Enter first name: ");
-		String firstName = "Harsh"; //scan.nextLine();
-		
-		matcher = firstNamePattern.matcher(firstName);
+		matcher = firstNamePattern.matcher(name);
 		boolean matchFound = matcher.find();
-		if(matchFound) {
-			System.out.println(firstName+" -\t Valid");
-		} 
-		else {
-			System.out.println(firstName+" -\t Invalid");
-		}
+		return matchFound;
 		
 	}
 
