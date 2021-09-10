65749666e38601f05b742a1771286b5698abe5ad
Harsh R Jain <rjainharsh@gmail.com>
2021-09-10 19:57:12 +0530

    ADDED: class to create custom exception

---
+++ lib/src/main/java/com/bridgelabz/userregistrationregex/UserRegisterationException.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.userregistrationregex;
+
+public class UserRegisterationException extends Exception {
+	
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY, INVALID_NAME, INVALID_EMAIL, INVALID_PHONE_NUMBER, INAVLID_PASSWORD,
+		VALID_NAME, VALID_EMAIL, VALID_PHONE_NUMBER, VALID_PASSWORD
+	}
+	
+	ExceptionType type;
+	
+	public UserRegisterationException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
